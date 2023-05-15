b4dcda404abdb96b75b4c5f2a2ebbc5fecdbb497
senghon <ecovet91@naver.com>
2023-05-15 18:45:56 +0900

    today_patients_list update

--- main.py
+++ main.py
@@ -10,22 +10,41 @@ app = FastAPI()
 async def response_from_b():
     return {"message": "This is a response from B 컴퓨터."}
 
+##오늘의 건강검진 환자 불러오기
+@app.get("/today_patients_list")
+def today_patients_list():
+    with engine.connect() as conn:
+#         # dbo.hpl에서 hplptid, hplrdt,hplstf,hplvsid 추출
+        query1 = text("""
+            SELECT hplptid,hplrdt,hplstf,hplvsid
+            FROM dbo.hpl
+            WHERE CONVERT(date, hplrdt) = '2022-06-20' AND hplplid = 12000
+        """)
+        result1 = conn.execute(query1)
+        rows1 = result1.fetchall()
 
-@app.get("/get_table_count")
-async def get_table_count():
-    # B 컴퓨터의 MSSQL 데이터베이스에 연결
-    with pymssql.connect(
-        server="localhost",
-        # user="데이터베이스_사용자명",
-        # password="데이터베이스_비밀번호",
-        database="efmain"
-    ) as conn:
-        with conn.cursor() as cursor:
-            # 테이블 개수를 쿼리합니다.
-            cursor.execute("SELECT COUNT(*) FROM INFORMATION_SCHEMA.TABLES")
-            table_count = cursor.fetchone()[0]
-    return {"table_count": table_count}
+        # dbo.pt에서 ptid, ptname, ptsxid, ptclid, ptbrid 추출
+        pt_info = []
+        for row in rows1:
+            query2 = text(f"""
+                SELECT ptid, ptname, ptsxid, ptclid, ptbrid
+                FROM dbo.pt
+                WHERE ptid = '{row[0]}'
+            """)
+            result2 = conn.execute(query2)
+            row2 = result2.fetchone()
+            pt_info.append({
+                "ptid": row2[0],
+                "ptname": row2[1].encode('ISO-8859-1').decode('cp949').strip(),
+                "ptsxid": find_xid(row2[2]),
+                "ptclid": find_clid(row2[3]),
+                "ptbrid": find_brid(row2[4]),
+                "hplrdt": str(row[1]), # datetime 객체를 str로 변환
+                "emname":find_staf(row[2]),
+                "vsid" : row[3] #visd는 row의 3번째에 위치.
+            })
 
+        return pt_info
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8010)
